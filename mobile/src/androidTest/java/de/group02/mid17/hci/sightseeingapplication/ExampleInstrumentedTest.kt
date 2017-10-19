125cf251ba481214e63b1062a875623cff4388c9
Dennis Rachui <dennis.rachui@online.de>
2017-10-19 14:32:31 +0200

    Empty Project

---
+++ mobile/src/androidTest/java/de/group02/mid17/hci/sightseeingapplication/ExampleInstrumentedTest.kt
@@ -0,0 +1,24 @@
+package de.group02.mid17.hci.sightseeingapplication
+
+import android.support.test.InstrumentationRegistry
+import android.support.test.runner.AndroidJUnit4
+
+import org.junit.Test
+import org.junit.runner.RunWith
+
+import org.junit.Assert.*
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
+    @Test
+    fun useAppContext() {
+        // Context of the app under test.
+        val appContext = InstrumentationRegistry.getTargetContext()
+        assertEquals("de.group02.mid17.hci.sightseeingapplication", appContext.packageName)
+    }
+}
