a8c850f9219cb83325c5829287de77f0c4aefad4
dera <nice.fake.mail@web.de>
2017-12-14 17:54:36 +0100

    Added Kotlin Support and morphed the simple StartActivity to a Kotlin file.

---
+++ mobile/src/main/java/com/example/de/testssapplication/StartActivity.kt
@@ -0,0 +1,29 @@
+package com.example.de.testssapplication
+
+import android.content.Intent
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.widget.Button
+
+/**
+ * This is a very simple Activity which just displays a title screen and
+ * fires up a new Configuration Activity if its button is clicked.
+ */
+class StartActivity : AppCompatActivity() {
+
+    private var startTour: Button? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_start)
+
+        startTour = findViewById(R.id.button)
+        startTour!!.setOnClickListener {
+            finish()
+            val i = Intent(applicationContext, ConfigurationActivity::class.java)
+            //For Testing Watch Sender/Receiver
+            //Intent i = new Intent(getApplicationContext(), TestWatchSending.class);
+            startActivity(i)
+        }
+    }
+}
